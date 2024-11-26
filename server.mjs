99c9349202d921a4dd8fe0e59c89ae8406628ebb
Murshyne <murshyne@gmail.com>
2024-11-26 02:43:05 -0500

    Committed at: Tue, Nov 26, 2024  2:43:04 AM

--- server.mjs
+++ server.mjs
@@ -18,9 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'x-auth-token'],
+  allowedHeaders: ['Content-Type', 'x-auth-token', 'Authorization'],
   credentials: true,
 }));
+
+app.options('*', cors());
 
 // Test route
 app.get('/', (req, res) => res.send('It is Working !!! Api Running'));
@@ -30,6 +32,10 @@ app.use('/api/auth', authRoutes);
 app.use('/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/auth', userRoutes);
+app.delete('/api/users/delete', (req, res) => {
+  
+});
+
 
 // Listener
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
